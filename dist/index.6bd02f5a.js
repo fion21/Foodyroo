93d9444bac137a20df5855890741f7be55029467
Fiona Lewis <fionaapple@Fionas-MacBook-Pro.local>
2024-02-17 21:30:31 +0000

    episode-03

--- dist/index.6bd02f5a.js
+++
@@ -1,9 +0,0 @@
-const heading = React.createElement("h1", {
-    id: "heading",
-    xyz: "abc"
-}, "Hello World from React!");
-console.log(heading); // object
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
-
-//# sourceMappingURL=index.6bd02f5a.js.map
