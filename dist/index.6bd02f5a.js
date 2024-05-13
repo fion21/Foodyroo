330fe4662aeb07a8f63a9e9b5f06e65c85b23fc5
Fiona Lewis <hwindepth@yahoo.com>
2024-05-13 17:29:47 +0100

    dist folder

---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,9 @@
+const heading = React.createElement("h1", {
+    id: "heading",
+    xyz: "abc"
+}, "Hello World from React!");
+console.log(heading); // object
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
